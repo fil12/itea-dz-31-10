56288f6ebccc21c3eb7a9c1a9ef4c6f4b0492932
filimonov <filimonov12@gmail.com>
2019-11-03 19:17:52 +0200

    added filters added filters

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,52 +1,82 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1 style="color:red">/* filters */</h1>
-      </div>
-      <card v-for="news in newsArr" :key="`news_${news.id}`" :news="news"></card>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 style="color:red">/* filters */</h1>
+            </div>
+            <card v-for="news in newsArr" :key="`news_${news.id}`" :news="news"></card>
+        </div>
+        <my-input
+                v-model="FilterForm.element"
+                placeholder="Введите кол-во элементов"
+                type="text"/>
+        <p>Вы ввели {{FilterForm.element}} элемент{{FilterForm.element | formatEnding(endings)}}</p>
     </div>
-  </div>
-  
+
 </template>
 
 <script>
-  import Card from './Card'
+    import Card from './Card'
+    import MyInput from "./MyInput";
 
-  export default {
-    name: "Filters",
-    components: { Card },
-    data() {
-      return {
-        newsArr: [
-          {
-            id: 1,
-            date: '2019-06-30T18:09:01+00:00',
-            title: 'news 1',
-            desc: 'description of news 1',
-            img: 'https://media.wsls.com/photo/2017/04/24/Whats%20News%20Today_1493062809311_9576980_ver1.0_1280_720.png',
-            link: '#'
-          },
-          {
-            id: 2,
-            date: '2018-03-15T18:08:04+00:00',
-            title: 'news 2',
-            desc: 'description of news 2',
-            img: '',
-            link: '#'
-          },
-          {
-            id: 3,
-            date: '2016-08-22T18:08:04+00:00',
-            title: 'news 3',
-            desc: 'description of news 3',
-            img: 'https://www.ctvnews.ca/polopoly_fs/1.4344128.1553095687!/httpImage/image.png_gen/derivatives/landscape_620/image.png',
-            link: '#'
-          }
-        ]
-      }
+    export default {
+        name: "Filters",
+        components: {Card, MyInput},
+        filters: {
+            formatEnding(val, arr) {
+                arr.forEach(function (elem, index) {
+                    console.log(elem);
+                    if (elem.includes(val)){
+                    }
+                })
+            }
+        },
+        data() {
+            return {
+                FilterForm: {
+                    element: null
+                },
+
+                endings: [
+                    [
+                       'ов', 5, 6, 7, 8, 9,
+                    ],
+                    [
+                        'а', 2, 3, 4,
+                    ],
+                    [
+                        '', 1
+                    ]
+                ],
+                newsArr: [
+                    {
+                        id: 1,
+                        date: '2019-06-30T18:09:01+00:00',
+                        title: 'news 1',
+                        desc: 'description of news 1',
+                        img: 'https://media.wsls.com/photo/2017/04/24/Whats%20News%20Today_1493062809311_9576980_ver1.0_1280_720.png',
+                        link: '#'
+                    },
+                    {
+                        id: 2,
+                        date: '2018-03-15T18:08:04+00:00',
+                        title: 'news 2',
+                        desc: 'description of news 2',
+                        img: '',
+                        link: '#'
+                    },
+                    {
+                        id: 3,
+                        date: '2016-08-22T18:08:04+00:00',
+                        title: 'news 3',
+                        desc: 'description of news 3',
+                        img: 'https://www.ctvnews.ca/polopoly_fs/1.4344128.1553095687!/httpImage/image.png_gen/derivatives/landscape_620/image.png',
+                        link: '#'
+                    }
+                ]
+            }
+        },
     }
-  }
 </script>
 
 <style lang="sass">
---
+++ src/components/MyInput.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="form-group">
+        <input
+                :type="type"
+                :placeholder="placeholder"
+                :class = "NameClass"
+                @input="$emit('input', $event.target.value)"
+        >
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyInput",
+        props:{
+            type: String,
+            placeholder: String,
+            NameClass: {type: String, default: `form-control`}
+        },
+
+        data() {
+            return {
+                FormData: {
+
+                }
+            }
+        },
+
+        filters: {
+            formatEnding(val) {
+                console.log(val);
+                // this.endingsArr.forEach(function (element, index) {
+                //   // console.log(val);
+                //   console.log(index);
+                //   console.log(element);
+                //     if (element.includes(val)) {
+                //
+                //
+                //       return index;
+                //     }
+                // });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
